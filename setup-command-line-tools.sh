800e5aaff50bf1977ef6f461065228df6c5740fb
István Hetesi <istvan.hetesi@nevis-security.com>
2023-03-24 12:00:20 +0100

    Generating help text for all the plugins in one step.

--- setup-command-line-tools.sh
+++ setup-command-line-tools.sh
@@ -5,168 +5,172 @@ set -euo pipefail
 start_directory=
 
 main() {
-	start_directory=$(pwd)
+    start_directory=$(pwd)
 
-	setup_bash
-	setup_vim
-	setup_nvim
-	setup_tmux
+    setup_bash
+    setup_vim
+    setup_nvim
+    setup_tmux
 
-	cat <<- EOF
+    cat <<-EOF
 	################################################################################
 	#### Manual steps, please read those instructions carefully!
 	################################################################################
 	EOF
 
-	print_tmux_manual_instructions
+    print_tmux_manual_instructions
 }
 
 setup_bash() {
-	cat <<- EOF
+    cat <<-EOF
 	################################################################################
 	#### Setting up common Bash settings
 	################################################################################
 	EOF
 
-	cp bash_common_settings ~/
+    cp bash_common_settings ~/
 
-	local source_instruction source_segment
-	source_instruction=". ~/bash_common_settings"
-	source_segment="\n## Sourcing commonly used Bash settings\n${source_instruction}\n"
+    local source_instruction source_segment
+    source_instruction=". ~/bash_common_settings"
+    source_segment="\n## Sourcing commonly used Bash settings\n${source_instruction}\n"
 
-	if ! grep -q "${source_instruction}" ~/.bashrc; then
-		echo -e "${source_segment}" >> ~/.bashrc
-	fi
+    if ! grep -q "${source_instruction}" ~/.bashrc; then
+        echo -e "${source_segment}" >>~/.bashrc
+    fi
 
-	if [[ $(uname -s) == "Darwin" ]]; then
-		if ! grep -q "${source_instruction}" ~/.bash_profile; then
-			echo -e "${source_segment}" >> ~/.bash_profile
-		fi
-	fi
+    if [[ $(uname -s) == "Darwin" ]]; then
+        if ! grep -q "${source_instruction}" ~/.bash_profile; then
+            echo -e "${source_segment}" >>~/.bash_profile
+        fi
+    fi
 
-	echo -e "#### Done ✅\n"
+    echo -e "#### Done ✅\n"
 }
 
 setup_vim() {
-	cat <<- EOF
+    cat <<-EOF
 	################################################################################
 	#### Setting up vim
 	################################################################################
 	EOF
 
-	# Placing config to it's place
-	cp ./.vimrc ~/.vimrc
-	mkdir -p ~/.vim
+    # Placing config to it's place
+    cp ./.vimrc ~/.vimrc
+    mkdir -p ~/.vim
 
-	# Pathogen installation
-	mkdir -p ~/.vim/autoload
-	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+    # Pathogen installation
+    mkdir -p ~/.vim/autoload
+    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-	# Fetching plugin repositories
-	git_clone_or_update ~/.vim/bundle/vim-colors-solarized https://github.com/altercation/vim-colors-solarized.git
-	git_clone_or_update ~/.vim/pack/airblade/start/vim-gutter https://github.com/airblade/vim-gitgutter.git
-	git_clone_or_update ~/.vim/pack/git-plugins/start/ale https://github.com/dense-analysis/ale.git
-	git_clone_or_update ~/.vim/pack/dist/start/vim-airline https://github.com/vim-airline/vim-airline
+    # Fetching plugin repositories
+    git_clone_or_update ~/.vim/bundle/vim-colors-solarized https://github.com/altercation/vim-colors-solarized.git
+    git_clone_or_update ~/.vim/pack/airblade/start/vim-gutter https://github.com/airblade/vim-gitgutter.git
+    git_clone_or_update ~/.vim/pack/git-plugins/start/ale https://github.com/dense-analysis/ale.git
+    git_clone_or_update ~/.vim/pack/dist/start/vim-airline https://github.com/vim-airline/vim-airline
 
-	# Plugin initialisation
-	vim -u NONE -c "helptags ~/.vim/pack/airblade/start/vim-gitgutter/doc" -c q
-	vim -u NONE -c "helptags ~/.vim/pack/dist/start/vim-airline/doc" -c q
+    # Plugin initialisation
+    vim -u NONE -c "helptags ~/.vim/pack/airblade/start/vim-gitgutter/doc" -c q
+    vim -u NONE -c "helptags ~/.vim/pack/dist/start/vim-airline/doc" -c q
 
-	echo -e "#### Done ✅\n"
+    echo -e "#### Done ✅\n"
 }
 
 setup_nvim() {
-	cat <<- EOF
+    cat <<-EOF
 	################################################################################
 	#### Setting up nvim
 	################################################################################
 	EOF
 
-	# Setting up configuration folder
-	local nvim_config_folder
-	nvim_config_folder="${HOME}/.config/nvim"
-	mkdir -p "${nvim_config_folder}"
+    # Setting up configuration folder
+    local nvim_config_folder
+    nvim_config_folder="${HOME}/.config/nvim"
+    mkdir -p "${nvim_config_folder}"
 
-	# Placing nvim configuration to the config folder
-	cp "${start_directory}/init.vim" "${nvim_config_folder}/"
+    # Placing nvim configuration to the config folder
+    cp "${start_directory}/init.vim" "${nvim_config_folder}/"
 
-	# Installing vim-plug
-	curl -sfLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-               https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    # Installing vim-plug
+    curl -sfLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-	# Installing JetBrains Mono Nerd font
-	if [[ $(uname -s) == "Darwin" ]]; then
-		brew tap homebrew/cask-fonts
-		brew install --cask font-jetbrains-mono
-		brew install --cask font-jetbrains-mono-nerd-font
-	else
-		local nerd_font_path home_font_path
-		nerd_font_path="/media/data/WorkSync/repositories/tools/nerd-fonts"
-		home_font_path="${HOME}/.fonts/truetype/JetBrainsMono"
-		mkdir -p "${home_font_path}"
-		git_clone_or_update "${nerd_font_path}" https://github.com/ryanoasis/nerd-fonts.git
-		find "${nerd_font_path}/patched-fonts/JetBrainsMono/Ligatures/" -type f -name '*.ttf' -exec cp {} "${home_font_path}/" \;
-	fi
+    # Installing JetBrains Mono Nerd font
+    if [[ $(uname -s) == "Darwin" ]]; then
+        brew tap homebrew/cask-fonts
+        brew install --cask font-jetbrains-mono
+        brew install --cask font-jetbrains-mono-nerd-font
+    else
+        local nerd_font_path home_font_path
+        nerd_font_path="/media/data/WorkSync/repositories/tools/nerd-fonts"
+        home_font_path="${HOME}/.fonts/truetype/JetBrainsMono"
+        mkdir -p "${home_font_path}"
+        git_clone_or_update "${nerd_font_path}" https://github.com/ryanoasis/nerd-fonts.git
+        find "${nerd_font_path}/patched-fonts/JetBrainsMono/Ligatures/" -type f -name '*.ttf' -exec cp {} "${home_font_path}/" \;
+    fi
 
-	nvim -u NONE -c "PlugInstall" -c "helptags vim-gitgutter/doc" -c "helptags vim-fugitive/doc" -c q
+    nvim -u NONE -c "PlugInstall" -c q
+    (cd ~/.config/nvim/plugged && nvim -u NONE -c "helptags ." -c q)
 
-	echo -e "#### Done ✅\n"
+    echo -e "#### Done ✅\n"
 }
 
 setup_tmux() {
-	cat <<- EOF
+    cat <<-EOF
 	################################################################################
 	#### Setting up tmux
 	################################################################################
 	EOF
 
-	cd "${start_directory}"
+    cd "${start_directory}"
 
-	# Placing config to it's place
-	cp ./.tmux.conf ~/.tmux.conf
-	mkdir -p ~/.tmux
+    # Placing config to it's place
+    cp ./.tmux.conf ~/.tmux.conf
+    mkdir -p ~/.tmux
 
-	# Ensuring that the plugins directory is located at the right place
-	local mount_line tmux_home_plugins
-	tmux_home_plugins=~/.tmux/plugins
-	if mount_line=$(mount | grep -E '/home' 2>/dev/null); then
-		if [[ ${mount_line} =~ noexec ]]; then
-			# Plugin directory needs to be reside outside the home directory since plugins can not be executed from there
-			local tmux_plugin_directory
-			tmux_plugin_directory=/opt/devtools/${USER}/tmux/plugins
+    # Ensuring that the plugins directory is located at the right place
+    local mount_line tmux_home_plugins
+    tmux_home_plugins=~/.tmux/plugins
+    if mount_line=$(mount | grep -E '/home' 2>/dev/null); then
+        if [[ ${mount_line} =~ noexec ]]; then
+            # Plugin directory needs to be reside outside the home directory since plugins can not be executed from there
+            local tmux_plugin_directory
+            tmux_plugin_directory=/opt/devtools/${USER}/tmux/plugins
 
-			if [[ ! -d "${tmux_plugin_directory}" ]]; then
-				sudo mkdir -p "${tmux_plugin_directory}"
-				sudo chown -R "${USER}":users "$(dirname "${tmux_plugin_directory}")"
-			fi
+            if [[ ! -d "${tmux_plugin_directory}" ]]; then
+                sudo mkdir -p "${tmux_plugin_directory}"
+                sudo chown -R "${USER}":users "$(dirname "${tmux_plugin_directory}")"
+            fi
 
-			if [[ -d "${tmux_home_plugins}" && ! -h "${tmux_home_plugins}" ]]; then
-				# Relocate already existing stuff (just for the case if something was already existing)
-				if [[ -n $(ls -1 "${tmux_home_plugins}") ]]; then
-					mv "${tmux_home_plugins}"/* "${tmux_plugin_directory}/"
-				fi
+            if [[ -d "${tmux_home_plugins}" && ! -L "${tmux_home_plugins}" ]]; then
+                # Relocate already existing stuff (just for the case if something was already existing)
+                if [[ -n $(ls -1 "${tmux_home_plugins}") ]]; then
+                    mv "${tmux_home_plugins}"/* "${tmux_plugin_directory}/"
+                fi
 
-				rmdir "${tmux_home_plugins}"
-			fi
+                rmdir "${tmux_home_plugins}"
+            fi
 
-			if [[ ! -h "${tmux_home_plugins}" ]]; then
-				(cd ~/.tmux; ln -s "${tmux_plugin_directory}" plugins)
-			fi
-		else
-			if [[ ! -d "${tmux_home_plugins}" ]]; then
-				mkdir -p "${tmux_home_plugins}"
-			fi
-		fi
-	fi
+            if [[ ! -L "${tmux_home_plugins}" ]]; then
+                (
+                    cd ~/.tmux
+                    ln -s "${tmux_plugin_directory}" plugins
+                )
+            fi
+        else
+            if [[ ! -d "${tmux_home_plugins}" ]]; then
+                mkdir -p "${tmux_home_plugins}"
+            fi
+        fi
+    fi
 
-	# Fetching plugin repositories
-	git_clone_or_update "${tmux_home_plugins}/tpm" https://github.com/tmux-plugins/tpm
+    # Fetching plugin repositories
+    git_clone_or_update "${tmux_home_plugins}/tpm" https://github.com/tmux-plugins/tpm
 
-	echo -e "#### Done ✅\n"
+    echo -e "#### Done ✅\n"
 }
 
 print_tmux_manual_instructions() {
-	cat <<- EOF
+    cat <<-EOF
 	# type this in terminal if tmux is already running
 	tmux source ~/.tmux.conf
 	# install the plugins:
@@ -175,16 +179,16 @@ print_tmux_manual_instructions() {
 }
 
 git_clone_or_update() {
-	local target_directory git_repository
-	target_directory="${1:-}"
-	git_repository="${2:-}"
+    local target_directory git_repository
+    target_directory="${1:-}"
+    git_repository="${2:-}"
 
-	if [[ -d "${target_directory}" ]]; then
-		cd "${target_directory}"
-		git pull
-	else
-		git clone --depth 1 "${git_repository}" "${target_directory}"
-	fi
+    if [[ -d "${target_directory}" ]]; then
+        cd "${target_directory}"
+        git pull
+    else
+        git clone --depth 1 "${git_repository}" "${target_directory}"
+    fi
 }
 
 main
